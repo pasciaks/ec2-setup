23014cf38bd0383a236f489e3ea817d1b4ba4d77
Blake Buck <blakebuck@users.noreply.github.com>
2017-06-14 21:56:16 -0400

    Create setup.sh

--- setup.sh
+++ setup.sh
@@ -24,55 +24,55 @@ $DOMAIN="$(echo $DOMAIN | tr '[A-Z]' '[a-z]')"
 
 echo $DOMAIN
 
-# Update apt-get
-echo "===== Updating apt-get ====="
-apt-get update
+# # Update apt-get
+# echo "===== Updating apt-get ====="
+# apt-get update
 
-# Install NodeJS and Node Package Manager
-apt-get install nodejs
-apt-get install npm
+# # Install NodeJS and Node Package Manager
+# apt-get install nodejs
+# apt-get install npm
 
-# Create symbolic link for node
-ln -s /usr/bin/nodejs /usr/bin/node
+# # Create symbolic link for node
+# ln -s /usr/bin/nodejs /usr/bin/node
 
-# Remove non-alpha characters
-$USERNAME=$(echo $DOMAIN | sed 's/[^a-z]//g')
+# # Remove non-alpha characters
+# $USERNAME=$(echo $DOMAIN | sed 's/[^a-z]//g')
 
-# Add user
-adduser $USERNAME --gecos "" --disabled-password
+# # Add user
+# adduser $USERNAME --gecos "" --disabled-password
 
-cd /home/$USERNAME
-mkdir .ssh
-touch .ssh/authorized_keys
-chown -R $USERNAME:$USERNAME .ssh
-chmod 0700 .ssh
-chmod 0600 .ssh/authorized_keys
+# cd /home/$USERNAME
+# mkdir .ssh
+# touch .ssh/authorized_keys
+# chown -R $USERNAME:$USERNAME .ssh
+# chmod 0700 .ssh
+# chmod 0600 .ssh/authorized_keys
 
-wget https://risingempiretech.github.io/ec2-setup/publickey.pem
-cat publickey.pem >> .ssh/authorized_keys
-rm publickey.pem
+# wget https://risingempiretech.github.io/ec2-setup/publickey.pem
+# cat publickey.pem >> .ssh/authorized_keys
+# rm publickey.pem
 
-service ssh restart
+# service ssh restart
 
-wget https://dl.eff.org/certbot-auto
-chmod a+x ./certbot-auto
+# wget https://dl.eff.org/certbot-auto
+# chmod a+x ./certbot-auto
 
-./certbot-auto certonly --standalone --agree-tos --email blake@risingempire.tech -d $DOMAIN
+# ./certbot-auto certonly --standalone --agree-tos --email blake@risingempire.tech -d $DOMAIN
 
-mkdir public
+# mkdir public
 
-apt-get install nginx
+# apt-get install nginx
 
-wget https://risingempiretech.github.io/ec2-setup/nginx.config
-sed -i "s/DOMAIN/${DOMAIN}/g" nginx.config
-sed -i "s/USERNAME/${USERNAME}/g" nginx.config
-mv ./nginx.config /etc/nginx/sites-enabled/default
+# wget https://risingempiretech.github.io/ec2-setup/nginx.config
+# sed -i "s/DOMAIN/${DOMAIN}/g" nginx.config
+# sed -i "s/USERNAME/${USERNAME}/g" nginx.config
+# mv ./nginx.config /etc/nginx/sites-enabled/default
 
-service nginx restart
+# service nginx restart
 
-crontab -l > oldcron
+# crontab -l > oldcron
 
-echo "24 02 * * * /home/$USERNAME/certbot-auto renew" >> oldcron
+# echo "24 02 * * * /home/$USERNAME/certbot-auto renew" >> oldcron
 
-crontab oldcron
-rm oldcron
+# crontab oldcron
+# rm oldcron
