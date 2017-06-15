9faaf31bdd4e6f30f8c5874719377af539728242
Blake Buck <blakebuck@users.noreply.github.com>
2017-06-14 22:15:06 -0400

    Create setup.sh

--- setup.sh
+++ setup.sh
@@ -29,18 +29,23 @@ echo "===== Updating apt-get ====="
 apt-get update
 
 # Install NodeJS and Node Package Manager
-apt-get install nodejs
-apt-get install npm
+echo "===== Installing Node ====="
+apt-get -y install nodejs
+echo "===== Installing NPM ====="
+apt-get -y install npm
 
 # Create symbolic link for node
+echo "===== Create Symbolic Link ====="
 ln -s /usr/bin/nodejs /usr/bin/node
 
 # Remove non-alpha characters
 USERNAME=$(echo $DOMAIN | sed 's/[^a-z]//g')
 
 # Add user
+echo "===== Add New User ====="
 adduser $USERNAME --gecos "" --disabled-password
 
+echo "===== Create SSH Directory ====="
 cd /home/$USERNAME
 mkdir .ssh
 touch .ssh/authorized_keys
@@ -48,21 +53,25 @@ chown -R $USERNAME:$USERNAME .ssh
 chmod 0700 .ssh
 chmod 0600 .ssh/authorized_keys
 
+echo "===== Get Public Key ====="
 wget https://risingempiretech.github.io/ec2-setup/publickey.pem
 cat publickey.pem >> .ssh/authorized_keys
 rm publickey.pem
 
 service ssh restart
 
+echo "===== Download Certbot ====="
 wget https://dl.eff.org/certbot-auto
 chmod a+x ./certbot-auto
 
-./certbot-auto certonly --standalone --agree-tos --email blake@risingempire.tech -d $DOMAIN
+./certbot-auto certonly --non-interactive --standalone --agree-tos --email blake@risingempire.tech -d $DOMAIN
 
 mkdir public
 
-apt-get install nginx
+echo "===== Install NGINX ====="
+apt-get -y install nginx
 
+echo "===== Download NGINX Config ====="
 wget https://risingempiretech.github.io/ec2-setup/nginx.config
 sed -i "s/DOMAIN/${DOMAIN}/g" nginx.config
 sed -i "s/USERNAME/${USERNAME}/g" nginx.config
@@ -70,6 +79,8 @@ mv ./nginx.config /etc/nginx/sites-enabled/default
 
 service nginx restart
 
+echo "===== Switch User ====="
 su $USERNAME
 
+echo "===== Create Cron Job ====="
 (crontab -l 2>/dev/null; echo "24 02 * * * /home/$USERNAME/certbot-auto renew") | crontab -
